b710d1d8601046940fa282ac523b5f8d4d9c3104
Ardhi Putra <ardhiputrapradana21@gmail.com>
2023-10-19 11:21:13 +0700

    feat: create api list talents

--- src/app/api/talents/route.ts
+++ src/app/api/talents/route.ts
@@ -3,6 +3,40 @@ import { parseZodErrors } from '@/utils/zodUtils';
 import { CreateTalentValidator } from '@/validators/talentValidator';
 import { NextRequest, NextResponse } from 'next/server';
 
+export const GET = async (req: NextRequest) => {
+  const params = req.nextUrl.searchParams;
+  const search = params.get('search');
+  const page = Number(params.get('page')) || 1;
+  const limit = Number(params.get('limit')) || 10;
+
+  try {
+    const count = await db.talent.count({ where: { name: { contains: search } } });
+
+    const talents = await db.talent.findMany({
+      where: { name: { contains: search } },
+      take: limit,
+      skip: (page - 1) * limit,
+    });
+
+    const meta = {
+      total: count,
+      page,
+      limit,
+      totalPages: Math.ceil(count / limit),
+    };
+
+    return NextResponse.json({ success: true, message: 'Talents fetched.', data: talents, meta });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: 'Error while fetching talents, try again later.',
+      },
+      { status: 500 }
+    );
+  }
+};
+
 export const POST = async (req: NextRequest) => {
   try {
     const body = await req.json();
