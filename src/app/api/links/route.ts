b5d35fd99a12b83dcc8e18a48a1da7f0585b505f
Ardhi Putra <ardhiputrapradana21@gmail.com>
2023-10-31 14:29:48 +0700

    feat: create list links api

--- src/app/api/links/route.ts
+++ src/app/api/links/route.ts
@@ -3,6 +3,41 @@ import { parseZodErrors } from '@/utils/zodUtils';
 import { CreateLinkValidator } from '@/validators/linkValidator';
 import { NextRequest, NextResponse } from 'next/server';
 
+export const GET = async (req: NextRequest) => {
+  const params = req.nextUrl.searchParams;
+  const search = params.get('search') || '';
+  const page = Number(params.get('page')) || 1;
+  const limit = Number(params.get('limit')) || 10;
+
+  try {
+    const count = await db.link.count({ where: { slug: { contains: search } } });
+
+    const links = await db.link.findMany({
+      where: { slug: { contains: search } },
+      take: limit,
+      skip: (page - 1) * limit,
+      orderBy: { createdAt: 'desc' },
+    });
+
+    const meta = {
+      total: count,
+      page,
+      limit,
+      totalPages: Math.ceil(count / limit),
+    };
+
+    return NextResponse.json({ success: true, message: 'Links fetched.', data: links, meta });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: 'Error while fetching links, try again later.',
+      },
+      { status: 500 }
+    );
+  }
+};
+
 export const POST = async (req: NextRequest) => {
   try {
     const validate = CreateLinkValidator.safeParse(await req.json());
